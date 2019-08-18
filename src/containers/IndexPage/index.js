757669bad009aad7bac948ac84f8ccb9d7ff93e5
sherry <sherry@deepcodify.com>
2019-08-18 22:58:56 +0800

    forum cover

--- src/containers/IndexPage/index.js
+++ src/containers/IndexPage/index.js
@@ -1,10 +1,13 @@
-import Link from 'next/link';
-import styled from 'styled-components';
+import Link from "next/link";
+import styled from "styled-components";
 
 const Card = styled.div`
   width: 480px;
   height: 640px;
   border: solid 1px black;
+  background-color: hsl(215, 62%, 21%);
+  overflow: hidden;
+  border-radius: 30px;
 `;
 
 const A = styled.a``;
@@ -15,7 +18,7 @@ const Wrapper = styled.div`
   align-items: center;
   position: relative;
   min-height: 100vh;
-  
+
   ${A} {
     position: absolute;
     top: 0;
@@ -23,10 +26,47 @@ const Wrapper = styled.div`
   }
 `;
 
+const ChurchImg = styled.img`
+  margin-top: 20%;
+  margin-left: 5%;
+  margin-right: 5%;
+`;
+
+const TextContainer = styled.div`
+  color: #fff;
+  width: 80%;
+  text-align: center;
+  margin: auto;
+  padding-top: 20px;
+`;
+
+const ChurchText = styled.div`
+  font-size: 40px;
+`;
+
+const NameText = styled.div`
+  font-size: 50px;
+  padding-top: 20px;
+  text-indent: 50px;
+  letter-spacing: 50px;
+`;
+
+const EkkImg = styled.img`
+  margin-top: 15%;
+  margin-left: 5%;
+`;
+
 const IndexPage = () => (
   <Wrapper>
-    <Card />
-    <Link href='/about' passHref>
+    <Card>
+        <ChurchImg src="/static/logo.svg" alt="Ekk" />
+        <TextContainer>
+          <ChurchText>新生命小組教會</ChurchText>
+          <NameText>高婕苓</NameText>
+        </TextContainer>
+        <EkkImg src="/static/ekk.svg" alt="Ekk" />
+    </Card>
+    <Link href="/about" passHref>
       <A>About Page</A>
     </Link>
   </Wrapper>
